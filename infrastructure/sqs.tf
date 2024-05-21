770f42b3a83a00c961aecd542ff060766beebe3e
Lurickardo <luizr726@gmail.com>
2024-05-20 23:15:12 -0300

    fix: adjust update status booking and remove sqs-payment

--- infrastructure/sqs.tf
+++ infrastructure/sqs.tf
@@ -1,14 +1,3 @@
-module "sqs-payment" {
-  source  = "terraform-aws-modules/sqs/aws"
-  version = "4.2.0"
-
-  name = "paymentVouchers"
-
-  fifo_queue = true
-
-  tags = local.tags
-}
-
 module "sqs-bookings" {
   source  = "terraform-aws-modules/sqs/aws"
   version = "4.2.0"
